fed478cfd3b79caebffa6e589f514f4d1b5cbbed
Shanice Brown <s.mariebrown10@gmail.com>
2021-05-13 12:23:58 -0500

    Initial Commit

---
+++ Templetes/TempletesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempletesApp.swift
+//  Templetes
+//
+//  Created by Westside Health Authority on 5/13/21.
+//
+
+import SwiftUI
+
+@main
+struct TempletesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
